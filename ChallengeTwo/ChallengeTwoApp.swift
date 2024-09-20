63d755c4ac3a0d983d0d4d8f88781fed6f791df7
Adam <tabievadam@gmail.com>
2024-09-20 16:41:58 +0200

    Initial Commit

---
+++ ChallengeTwo/ChallengeTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeTwoApp.swift
+//  ChallengeTwo
+//
+//  Created by Адам Табиев on 20.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
